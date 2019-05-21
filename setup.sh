b3e4dcc444e89b98fe9e907d3080f644c5fcd4c9
macbookpr02010 <macbookpro2010@gianduia.net>
2019-05-21 16:48:56 +0200

    rubinius compiler script

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+locale-gen en_US.UTF-8
+apt-get update
+apt-get upgrade
+apt-get install make clang-3.6 g++ ruby-dev libncurses-dev zlib1g-dev libssl-dev libedit-dev libncursesw5-dev git
+ln -s /usr/bin/llvm-config-3.6 /usr/bin/llvm-config
+mkdir -p /home/pi
+cd /home/pi
+git clone https://github.com/medols/rubinius.git
+cd rubinius
+LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 LC_ALL=en_US.UTF-8 ./configure --prefix=/opt/rubinius/4.0 --llvm-config=llvm-config-3.6 --cc=clang-3.6 --cxx=clang++-3.6
+LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 LC_ALL=en_US.UTF-8 rake
