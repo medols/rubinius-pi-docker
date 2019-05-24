6cd11b7b4d73b84ec2972606343fd8e065a02a99
macbookpr02010 <macbookpro2010@gianduia.net>
2019-05-24 11:51:51 +0200

    llvm-config

--- setup.sh
+++ setup.sh
@@ -9,5 +9,5 @@ mkdir -p /home/pi
 cd /home/pi
 git clone https://github.com/medols/rubinius.git
 cd rubinius
-LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 LC_ALL=en_US.UTF-8 ./configure --prefix=/opt/rubinius/4.0 --llvm-config=llvm-config-3.6 --cc=clang-3.6 --cxx=clang++-3.6
+LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 LC_ALL=en_US.UTF-8 ./configure --prefix=/opt/rubinius/4.0 --llvm-config=llvm-config
 LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 LC_ALL=en_US.UTF-8 rake
